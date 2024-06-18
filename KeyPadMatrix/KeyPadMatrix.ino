75ba2b48ed5978efc33116c4957c44ce49aaefa0
arjjung29 <arjjungobalakrishnan@gmail.com>
2024-06-18 23:44:07 +0530

    This contains code that reads and displays the key pressed in a 4x4 Matrix

---
+++ KeyPadMatrix/KeyPadMatrix.ino
@@ -0,0 +1,39 @@
+#include <Keypad.h>
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four  columns
+char keys[ROWS][COLS] = { 
+{'1','2','3','A'}, 
+{'4','5','6','B'}, 
+{'7','8','9','C'}, 
+{'*','0','#','D'} 
+}; 
+byte rowPins[ROWS] = {5, 4, 3,2}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {11,10,9,8}; //connect to the column pinouts of the keypad
+//Create an object of keypad
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+String input =""; 
+
+void setup()
+{ 
+Serial.begin(9600); 
+} 
+void loop()
+{ 
+char key = keypad.getKey();// Read the key
+// Print if key pressed
+if (key)
+{ 
+Serial.print("Key Pressed : "); 
+Serial.println(key); 
+}
+if (key){ 
+ if (key =='#'){
+  Serial.println(input);
+  input="";
+}
+ else{
+  input+=key;
+}
+}
+}

b301613d9b519af9cfbe2e8eef6a53f746727368
arjjung29 <arjjungobalakrishnan@gmail.com>
2024-06-20 01:38:41 +0530

    Fully working code with comments next to implement number of attempts and lockout / GSM Alert Feature

--- KeyPadMatrix-Password/KeyPadMatrix-Password.ino
+++ KeyPadMatrix-Password/KeyPadMatrix-Password.ino
@@ -11,40 +11,40 @@ char keys[ROWS][COLS] = {
 byte rowPins[ROWS] = {5, 4, 3, 2}; // Connect to the row pinouts of the keypad
 byte colPins[COLS] = {11, 10, 9, 8}; // Connect to the column pinouts of the keypad
 
-Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS); // Initializing the Keypad object
 
 String password = "1234"; // Default password
-String input = "";
-int count = 0;
+String input = ""; //User's Input
+int count = 0; //To loop over the user's input until the max length of password is reached
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); //Begin Serial communication for displaying in the Serial Monitor
 }
 
 void loop() {
   char key = keypad.getKey();
   if (key) {
-    if (key == 'A') {
+    if (key == 'A') { //Validation of your password or simply entering your password for access to be granted
       Serial.println("Enter Password: ");
-      input = "";
+      input = ""; // for storing user's input
       count = 0;
       while (count < 4) {
         char key = keypad.getKey();
         if (key) {
           Serial.print(key);
-          input += key;
+          input += key; // appending the keystrokes to the input string
           count++;
         }
       }
       Serial.println();
-      if (input == password) {
+      if (input == password) { // Checking for match (Validating)
         Serial.println("Password Matched!");
       } else {
         Serial.println("Password Incorrect!");
       }
     }
     
-    else if (key == 'B') {
+    else if (key == 'B') { //Case for Resetting your password
       Serial.println("Enter your old password: ");
       input = "";
       count = 0;
@@ -57,8 +57,8 @@ void loop() {
         }
       }
       Serial.println();
-      if (input == password) {
-        Serial.println("Password Reset Successfully! Press A to enter your new password.");
+      if (input == password) { //Validating if you're the previous person who set the last password
+        Serial.println("Password Reset Successfully! Enter your new password:"); // If success the user can enter their new password
         password = ""; // Clear the old password
         input = "";
         count = 0;
@@ -77,24 +77,6 @@ void loop() {
       }
     }
     
-    else if (key == 'C') {
-      Serial.println("Password Check!");
-      input = "";
-      count = 0;
-      while (count < 4) {
-        char key = keypad.getKey();
-        if (key) {
-          Serial.print(key);
-          input += key;
-          count++;
-        }
-      }
-      Serial.println();
-      if (input == password) {
-        Serial.println("Password Matched!");
-      } else {
-        Serial.println("Password Incorrect!");
-      }
-    }
+
   }
 }

f4dd13836be652bffe096004ee6ee6eee0ffcad7
arjjung29 <arjjungobalakrishnan@gmail.com>
2024-06-23 13:27:13 +0530

    Replaced code for OLED with 16x2 LCD since OLED Requires lot of RAM

--- KeyPadMatrix-Password/KeyPadMatrix-Password.ino
+++ KeyPadMatrix-Password/KeyPadMatrix-Password.ino
@@ -1,13 +1,7 @@
 #include <Keypad.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-
-#define SCREEN_WIDTH 128 
-#define SCREEN_HEIGHT 64 
-#define OLED_RESET     -1 
-#define SCREEN_ADDRESS 0x3C
+#include <LiquidCrystal_I2C.h>
 
 const byte ROWS = 4; // Four rows
 const byte COLS = 4; // Four columns
@@ -22,85 +16,101 @@ byte colPins[COLS] = {11, 10, 9, 8}; // Connect to the column pinouts of the key
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS); // Initializing the Keypad object
 SoftwareSerial GSM(7, 6); // SoftwareSerial(rxPin, txPin, inverse_logic), creating object for sending AT Commands to GSM Module
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// LCD Configuration
+LiquidCrystal_I2C lcd(0x3F, 16, 2); // Set the LCD I2C address and dimensions (16x2)
 
 String password = "1234"; // Default password
-String input = ""; //User's Input
-int count = 0; //To loop over the user's input until the max length of password is reached
+String input = ""; // User's Input
+int count = 0; // To loop over the user's input until the max length of password is reached
 int wrongAttempts = 0;
 
 void setup() {
-  Serial.begin(9600); //Begin Serial communication for displaying in the Serial Monitor
-  GSM.begin(9600); //Begin Serial communication with GSM for sending AT commands
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); 
-  }
-  display.clearDisplay();
+  Serial.begin(9600); // Begin Serial communication for displaying in the Serial Monitor
+  GSM.begin(9600); // Begin Serial communication with GSM for sending AT commands
+  lcd.init(); // Initialize the LCD
+  lcd.backlight(); // Turn on the backlight
+  lcd.clear(); // Clear the LCD screen
 }
 
 void loop() {
-
-  if (wrongAttempts >= 4) { //Cross check with number of wrong attempts so far
-    display.clearDisplay();
-    display.setCursor(0,0);  
-    display.setTextSize(1);
-    display.setTextColor(WHITE);
-    display.print("Too many wrong attempts! Please wait for 1 minute.");
+  if (wrongAttempts >= 4) { // Cross check with number of wrong attempts so far
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Too many wrong");
+    lcd.setCursor(0, 1);
+    lcd.print("attempts!");
+    delay(3000);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Wait for");
+    lcd.setCursor(0,1);
+    lcd.print("One minute");
+    delay(3000);
     Serial.println("Too many wrong attempts! Please wait for 1 minute.");
-    display.setCursor(1,0);
-    display.print("Calling the Registered Phone Number !");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Alerting....");
     Serial.println("Calling the Registered Phone Number !");
     callup(); // Call the registered mobile number and hang up after 20 seconds
     delay(60000); // Delay of 1 minute
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("One minute done");
+    lcd.setCursor(0,1);
+    lcd.print("You can Continue");
+    delay(2000);
+    lcd.clear();
     wrongAttempts = 0; // Reset the wrong attempts counter
-    display.clearDisplay();
+    lcd.clear();
   }
 
   char key = keypad.getKey();
   if (key) {
-    if (key == 'A') { //Validation of your password or simply entering your password for access to be granted
-      display.clearDisplay();
-      display.setCursor(0,0);  
-      display.setTextSize(1);
-      display.setTextColor(WHITE);
-      display.print("Enter Password: ");
+    lcd.clear();
+    if (key == 'A') { // Validation of your password or simply entering your password for access to be granted
+      lcd.setCursor(0, 0);
+      lcd.print("Enter Password:");
       Serial.println("Enter Password: ");
       input = ""; // for storing user's input
       count = 0;
+      lcd.setCursor(0,1);
       while (count < 4) {
         char key = keypad.getKey();
         if (key) {
           Serial.print(key);
           input += key; // appending the keystrokes to the input string
+          lcd.print(key); // display on LCD
           count++;
         }
       }
       Serial.println();
+      lcd.setCursor(0, 1);
       if (input == password) { // Checking for match (Validating)
         Serial.println("Password Matched!");
-        display.clearDisplay();
-        display.setCursor(0,0);  
-        display.setTextSize(1);
-        display.setTextColor(WHITE);
-        display.print("Password Matched !"); // printing to the OLED Display
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Password Matched!");
+        delay(2000);
+        lcd.clear();
       } else {
+        lcd.clear();
         Serial.println("Password Incorrect!");
-        display.clearDisplay();
-        display.setCursor(0,0);  
-        display.setTextSize(1);
-        display.setTextColor(WHITE);
-        display.print("Password Incorrect!");
-        wrongAttempts++; //Incrementing the wrong attempt counter
+        lcd.setCursor(0,0);
+        lcd.print("Password");
+        lcd.setCursor(0,1);
+        lcd.print("Incorrect");
+        delay(2000);
+        lcd.clear();
+        wrongAttempts++; // Incrementing the wrong attempt counter
       }
     }
     
-    else if (key == 'B') { //Case for Resetting your password
-      display.clearDisplay();
-      display.setCursor(0,0);  
-      display.setTextSize(1);
-      display.setTextColor(WHITE);
-      display.print("Enter your old Password: ");
+    else if (key == 'B') { // Case for Resetting your password
+      lcd.setCursor(0, 0);
+      lcd.print("Enter old");
+      lcd.setCursor(0,1);
+      lcd.print("Password:");
       Serial.println("Enter your old password: ");
       input = "";
       count = 0;
@@ -109,60 +119,64 @@ void loop() {
         if (key) {
           Serial.print(key);
           input += key;
+          lcd.print(key); // display on LCD
           count++;
         }
       }
       Serial.println();
-      if (input == password) { //Validating if you're the previous person who set the last password
-        display.clearDisplay();
-        display.setCursor(0,0);  
-        display.setTextSize(1);
-        display.setTextColor(WHITE);
-        display.print("Password Reset Successfully! Enter your new password:");
-        Serial.println("Password Reset Successfully! Enter your new password:"); // If success the user can enter their new password
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      if (input == password) { // Validating if you're the previous person who set the last password
+        lcd.print("Password Reset");
+        Serial.println("Password Reset Successfully! Enter your new password:");
+        lcd.setCursor(0, 1);
+        lcd.print("Successfully!");
+        delay(2000);
         password = ""; // Clear the old password
         input = "";
         count = 0;
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Enter New");
+        lcd.setCursor(0,1);
+        lcd.print("Password:");
         while (count < 4) {
           char key = keypad.getKey();
           if (key) {
             Serial.print(key);
             password += key;
+            lcd.print(key); // display on LCD
             count++;
           }
         }
         Serial.println();
-        Serial.println("New Password Set!");
-        display.clearDisplay();
-        display.setCursor(0,0);  
-        display.setTextSize(1);
-        display.setTextColor(WHITE);
-        display.print("New Password Set!");
-        
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("New Password Set!");
+        delay(2000);
+        lcd.clear();
       } else {
         Serial.println("Password doesn't match!");
-        display.clearDisplay();
-        display.setCursor(0,0);  
-        display.setTextSize(1);
-        display.setTextColor(WHITE);
-        display.print("Password doesn't match");
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Password doesn't");
+        lcd.setCursor(0,1);
+        lcd.print("Match !");
+        delay(2000);
+        lcd.clear();
         wrongAttempts++;
       }
     }
-    
-
   }
 }
 
 void update(){
   delay(500);
-  while (Serial.available()) 
-  {
-    GSM.write(Serial.read()); //Forward what Serial received to Software Serial Port
+  while (Serial.available()) {
+    GSM.write(Serial.read()); // Forward what Serial received to Software Serial Port
   }
-  while(GSM.available()) 
-  {
-    Serial.write(GSM.read()); //Forward what Software Serial received to Serial Port
+  while(GSM.available()) {
+    Serial.write(GSM.read()); // Forward what Software Serial received to Serial Port
   }
 }
 
@@ -172,9 +186,9 @@ void callup(){
 
   GSM.println("AT"); // If initialization is success GSM Module will return "OK"
   update();
-  GSM.println("ATD+ +916369727213;"); //  phone number to dial
+  GSM.println("ATD+916369727213;"); // phone number to dial
   update();
   delay(20000); // wait for 20 seconds...
-  GSM.println("ATH"); //hang up the call
+  GSM.println("ATH"); // hang up the call
   update();
 }
